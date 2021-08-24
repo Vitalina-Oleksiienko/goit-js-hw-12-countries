e4391cd45ae920821d05d4f2dd0618c1d860b275
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-08-24 13:09:01 +0300

    правки

--- src/index.js
+++ src/index.js
@@ -1,59 +1,55 @@
 import './sass/main.scss';
-
-import fetchCountries from '../src/fetchCountries.js';
-import countryMarkup from './templates/country-template.hbs';
-import countriesMarkup from './templates/countries-template.hbs';
-import { success, error, defaults } from '@pnotify/core/dist/PNotify.js';
+import { error, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
-import '@pnotify/confirm/dist/PNotifyConfirm.css';
+import debounce from 'lodash/debounce';
+import CountriesApiServise from './fetchCountries';
+import countryTpl from './templates/country.hbs';
+import countriesTpl from './templates/countries.hbs';
 
-defaults.delay = 2500;
 
-const debounse = require('lodash.debounce');
 const refs = {
-    inputEl: document.querySelector('.input'),
-    listEl: document.querySelector('.error'),
-};
+    searchInput: document.querySelector('.input-js'),
+    output: document.querySelector('.error')
+}
+const countriesApiServise = new CountriesApiServise();
 
-refs.inputEl.addEventListener('input', debounse(onInput, 500));
+refs.searchInput.addEventListener('input', debounce(onSearch, 500));
 
-function onInput(e) {
-    if (e.target.value.trim('') === '') {
-        refs.listEl.textContent = '';
-        return;
+function onSearch(e) {
+    e.preventDefault();
+
+    if (e.target.value.trim("") === "") { return };
+    refs.output.innerHTML = "";
+
+    countriesApiServise.query = e.target.value;
+    countriesApiServise.fetchCountries().then(countries => { countriesMarkup(countries) });
+    
+}
+
+function countriesMarkup(countries) {
+    console.log(countries);
+    const countriesLength = countries.length;
+    if (countriesLength === 1) {
+        refs.output.innerHTML = countryTpl(countries[0]);
+        refs.output.classList.toggle('position');
+        return
     }
 
-    refs.listEl.textContent = '';
-    fetchCountries(e.target.value.trim(''))
-        .then(response =>
-            response.ok ? response.json() : Promise.reject(response),
-        )
-        .then(countries => {
-            const countriesLength = countries.length;
-            if (countriesLength === 1) {
-                refs.listEl.innerHTML = countryMarkup(countries[0]);
-                success({ text: `Here's what you were looking for` });
-                return;
-            }
-            if (countriesLength > 1 && countriesLength <= 10) {
-                refs.listEl.innerHTML = countriesMarkup(countries);
-                success({ text: `Something went wrong` });
-                return;
-            }
-            error({
-                text: 'We found many countries, specify your request',
-            });
-        })
-        .catch(err => {
-            if (err.status === 404) {
-                error({
-                    text: `Error ${err.status}. There is no such country.`,
-                });
-            } else {
-                error({
-                    text: `Check your internet, we can't work like this :(`,
-                });
-            }
-        });
-        
+    if (countriesLength > 1 && countriesLength <= 10) {
+        refs.output.innerHTML = countriesTpl(countries);
+        refs.output.classList.toggle('position');
+        return
+    }
+
+    if (countriesLength > 10) {
+        error(
+            {
+                delay: 2000,  text: "Too much countries, clarify your request",  maxOpen: 1,
+            })
+        return
+    }
+    error(
+    {
+        delay: 2000,  text: "Not found", maxOpen: 1,
+    })
 }
